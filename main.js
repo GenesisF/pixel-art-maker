66c5ad23430c71e1b74c66043766e9f27adfcf96
Genesise <genesis.fadel17@gmail.com>
2018-09-24 18:03:05 -0600

    finsh my pixel art

---
+++ main.js
@@ -0,0 +1,46 @@
+(function(document){
+    
+    var canvas = document.querySelector('#canvas');
+
+    var colorCode;
+
+    for(var i = 0; i < 3500; i++){
+        
+        let square = document.createElement('div');
+        
+        square.classList.add('square');
+
+        canvas.appendChild(square);
+
+        square.addEventListener('click', function(e){
+          square.style.backgroundColor = colorCode;
+        });
+    }
+
+    //Color Palettes
+    // #f8bbd0  #ba6b6c  #78002e  #4dd0e1  #00897b
+
+    var palette = document.querySelector('#palette');
+
+    var colors = ['#f8bbd0', '#ba6b6c', '#78002e', '#4dd0e1', '#00897b'];
+    
+    for(var i = 0; i < colors.length; i++){
+
+        var color = document.createElement('div');
+
+        color.classList.add('color');
+        
+        let hex = colors[i];
+
+        color.style.backgroundColor = colors[i];
+
+        palette.appendChild(color);
+
+        color.addEventListener("click", function(e){
+            colorCode = hex;
+        });
+
+    }
+
+
+})(document);
